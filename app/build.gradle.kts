4b1eb2de3142458128ea5f0f0130d69542d1c57f
dusanvido <edu.sanvidoapolinario@gmail.com>
2025-04-24 22:50:43 -0300

    Firebase/banco de dados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -26,23 +27,29 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.common.ktx)
+    implementation("com.google.firebase-firestorektx:24.10.0")
+    implementation(libs.firebase.firestore.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
--- app/src/main/java/com/example/projeto_integrador3/MainActivity.kt
+++ app/src/main/java/com/example/projeto_integrador3/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.projeto_integrador3
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.`activity_mainLogin.xml`)
+        setContentView(R.layout.activity_main_login)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        FirebaseApp.initializeApp(this)
+        val db = FirebaseFirestore.getInstance()
+
+        val risco = hashMapOf(
+            "descricao" to "Fio desencapado próximo à escada",
+            "nivel" to "Alto",
+            "localizacao" to "Setor A, andar 2"
+        )
+
+        db.collection("riscos")
+            .add(risco)
+            .addOnSuccessListener { documentReference ->
+                Log.d("FIRESTORE", "Documento adicionado com ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("FIRESTORE", "Erro ao adicionar documento", e)
+            }
     }
 }
+
